c5222c0e7ed4658f9a292d8034a4e24368834ede
Nasser Setti <abdennasseressati@gmail>
2021-04-02 12:35:52 +0100

    chore: fill consultation page

--- src/modules/Consultation/index.tsx
+++ src/modules/Consultation/index.tsx
@@ -1,38 +1,80 @@
 import React from "react";
 import { AiOutlinePlus } from "react-icons/ai";
-import { Card } from "ui/Card";
+import Modal from "ui/CustomModal";
 import FabButton from "ui/FabButton";
 import Layout from "ui/Layout";
-import { Wrappper } from "./styles";
+import { IMedicament } from "utils";
+import { Header } from "./components/Header";
+import { Wrapper } from "./styles";
+const meds: IMedicament[] = [
+  {
+    id: 12,
+    forme:
+      "Lorem laborum ut ad officia deserunt duis irure eu consequat laborum non et.",
+    nom: "Non incididunt eiusmod non pariatur nostrud pariatur.",
+  },
+  {
+    id: 55612,
+    forme:
+      "Lorem laborum ut ad officia deserunt duis irure eu consequat laborum non et.",
+    nom: "Non incididunt eiusmod non pariatur nostrud pariatur.",
+  },
+  {
+    id: 1536,
+    forme:
+      "Lorem laborum ut ad officia deserunt duis irure eu consequat laborum non et.",
+    nom: "Non incididunt eiusmod non pariatur nostrud pariatur.",
+  },
+  {
+    id: 54512,
+    forme:
+      "Lorem laborum ut ad officia deserunt duis irure eu consequat laborum non et.",
+    nom: "Non incididunt eiusmod non pariatur nostrud pariatur.",
+  },
+  {
+    id: 545612,
+    forme:
+      "Lorem laborum ut ad officia deserunt duis irure eu consequat laborum non et.",
+    nom: "Non incididunt eiusmod non pariatur nostrud pariatur.",
+  },
+  {
+    id: 15366655,
+    forme:
+      "Lorem laborum ut ad officia deserunt duis irure eu consequat laborum non et.",
+    nom: "Non incididunt eiusmod non pariatur nostrud pariatur.",
+  },
+  {
+    id: 546554512,
+    forme:
+      "Lorem laborum ut ad officia deserunt duis irure eu consequat laborum non et.",
+    nom: "Non incididunt eiusmod non pariatur nostrud pariatur.",
+  },
+];
 export default function Consultations() {
-  // const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-  const handleClick = () => {
-    // setOpen((cur) => !cur);
+  const handleClose = () => {
+    setOpen(false);
   };
-  // const toggleModal = () => {
-  //   setOpen((cur) => !cur);
-  // };
+  const toggleModal = () => {
+    setOpen((cur) => !cur);
+  };
 
   return (
     <Layout>
-      <Wrappper>
-        <Card>Adipisicing qui sint laborum dolore non.</Card>
-        <Card>
-          Ea minim occaecat aliquip et cillum est dolore occaecat occaecat Lorem
-          magna aute.
-        </Card>
-        <Card>Nostrud officia voluptate fugiat incididunt esse aute.</Card>
-        <Card>Mollit consequat nostrud ex duis aliquip enim.</Card>
-        <Card>
-          Incididunt non consequat do consectetur labore mollit elit proident
-          ea.
-        </Card>
-      </Wrappper>
-
-      <FabButton onClick={handleClick}>
+      <Header />
+      <Wrapper>
+        {meds &&
+          meds.map((item) => {
+            return <p> {JSON.stringify(item)} </p>;
+          })}
+      </Wrapper>
+      <FabButton onClick={toggleModal}>
         <AiOutlinePlus size="25px" />
       </FabButton>
+      <Modal closeModal={handleClose} open={open}>
+        <p>Lhjkghhjh</p>
+      </Modal>
     </Layout>
   );
 }
--- src/modules/Consultation/styles.ts
+++ src/modules/Consultation/styles.ts
@@ -1,9 +1,12 @@
 import styled from "@emotion/styled";
 
-export const Wrappper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 32px;
-  padding: 16px;
-  justify-content: space-around;
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 300px));
+  justify-content: center;
+  align-items: center;
+  row-gap: 20px;
+  column-gap: 10px;
+  max-height: 80vh;
+  overflow-y: auto;
 `;
